ef32396e02043ac1d397137194cbe2036e2901b5
jmarca <o.marca25@gmail.com>
2020-04-05 17:03:10 -0400

    Update Title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -23,13 +19,15 @@ describe('AppComponent', () => {
   it(`should have as title 'FootballHeadzUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('FootballHeadzUI');
+    expect(app.title).toEqual('FootballHeadz');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to FootballHeadzUI!');
+    expect(compiled.querySelector('h1').textContent).toEqual(
+      'Welcome to FootballHeadz!'
+    );
   });
 });
